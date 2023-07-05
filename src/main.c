3e4a0e7c0b713c70dbe88a6efca68755aa6472a3
GreffMASTER <57036833+GreffMASTER@users.noreply.github.com>
2023-07-05 20:52:29 +0200

    Re-added legacy argument parser for Windows builds
    no gnulib on windows :(

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include "gbx.h"
-#include <argp.h>
 
-#define VER "1.0.8"
+
+#define VER "1.0.9"
 
 enum COMP_MODE {
     AUTO = 0,
@@ -14,55 +14,101 @@ uint8_t compmode = AUTO;
 char* inpath = NULL;
 char* outpath = NULL;
 
-const char *argp_program_version = "GBXLZO v" VER " by GreffMASTER";
-const char *argp_program_bug_address = ""; //Insert an appropriate E-mail address here
-static char doc[] = "A tool for decompressing/compressing GameBox files. (*.Gbx)";
-static char args_doc[] = "<infile>";
-static struct argp_option options[] = {
-    {"compress",   'c', 0,           0, "only compress"},
-    {"uncompress", 'u', 0,           0, "only decompress"},
-    {"output",     'o', "FILE", 0, "output path. If not provided, the input file will be used as output"},
-    {"verbose",    'v', 0,           0, "verbose"},
-    { 0 }
-};
+#ifdef _WIN32 // use legacy command line parser ( no gnulib on windows :( )
+    void prnt_help() {
+        printf("GBXLZO v%s by GreffMASTER\n", VER);
+        printf("A tool for decompressing/compressing GameBox files. (*.Gbx)\n");
+        printf("Usage: gbxlzo.exe <infile> [params]\n");
+        printf("Parameters:\n");
+        printf("-v         -  verbose\n");
+        printf("-c         -  only compress\n");
+        printf("-u         -  only decompress\n");
+        printf("-o <path>  -  output path\n");
+        printf("If output path is not provided, the input file will be used as output\n");
+        printf("By default, the program will decompress compressed files and vice versa\n");
+        printf("If you find any bugs, please contact me at mrdsdrdv@gmail.com\n");
+    }
+#elif __linux__ // linux
+    #include <argp.h>
 
-error_t argp_parser (int key, char *arg, struct argp_state *state){
-    switch(key){
-        case 'v':
-            verbose = true;
-            break;
+    const char *argp_program_version = "GBXLZO v" VER " by GreffMASTER";
+    const char *argp_program_bug_address = "mrdsdrdv@gmail.com";
+    static char doc[] = "A tool for decompressing/compressing GameBox files. (*.Gbx)";
+    static char args_doc[] = "<infile>";
+    static struct argp_option options[] = {
+        {"compress",   'c', 0,           0, "only compress"},
+        {"uncompress", 'u', 0,           0, "only decompress"},
+        {"output",     'o', "FILE", 0, "output path. If not provided, the input file will be used as output"},
+        {"verbose",    'v', 0,           0, "verbose"},
+        { 0 }
+    };
 
-        case 'c':
-            compmode = COMPRESS;
-            break;
+    error_t argp_parser (int key, char *arg, struct argp_state *state){
+        switch(key){
+            case 'v':
+                verbose = true;
+                break;
 
-        case 'd':
-            compmode = UNCOMPRESS;
-            break;
+            case 'c':
+                compmode = COMPRESS;
+                break;
 
-        case 'o':
-            outpath = arg;
-            break;
+            case 'd':
+                compmode = UNCOMPRESS;
+                break;
 
-        case ARGP_KEY_NO_ARGS: //No arguments
-            argp_usage(state); //No break, this exits
+            case 'o':
+                outpath = arg;
+                break;
 
-        case ARGP_KEY_ARG: //Input file name
-            inpath = arg;
-            state->next = state->argc;
-            break;
+            case ARGP_KEY_NO_ARGS: //No arguments
+                argp_usage(state); //No break, this exits
 
-        default:
-            return ARGP_ERR_UNKNOWN;
-    }       
-    return 0;
-}
+            case ARGP_KEY_ARG: //Input file name
+                inpath = arg;
+                state->next = state->argc;
+                break;
 
-static struct argp argp = { options, argp_parser, args_doc, doc, 0, 0, 0 };
+            default:
+                return ARGP_ERR_UNKNOWN;
+        }       
+        return 0;
+    }
+
+    static struct argp argp = { options, argp_parser, args_doc, doc, 0, 0, 0 };
+#endif
 
 int main(int argc, char** argv) {
-    
-    argp_parse(&argp, argc, argv, 0, 0, 0);
+    #ifdef _WIN32 // use legacy command line argument parser ( no gnulib on windows :( )
+        int requiredargs = 2;
+        if(argc<2) {
+            prnt_help();
+            return 0;
+        }
+        inpath = argv[1];
+        for(int i=2;i<argc;i++) {
+            if(strcmp(argv[i], "-v") == 0) {
+                requiredargs += 1;
+                verbose = true;
+            } else if(strcmp(argv[i], "-c") == 0) {
+                requiredargs += 1;
+                compmode = COMPRESS;
+            } else if(strcmp(argv[i], "-u") == 0) {
+                requiredargs += 1;
+                compmode = UNCOMPRESS;
+            } else if(strcmp(argv[i], "-o") == 0) {
+                requiredargs += 2;
+                if(argc<requiredargs) {
+                    printf("-o parameter requires a path");
+                    prnt_help();
+                    return 0;
+                }
+                outpath = argv[i+1];
+            }
+        }
+    #elif __linux__ // linux
+        argp_parse(&argp, argc, argv, 0, 0, 0);
+    #endif
     if(outpath==NULL) outpath = inpath; // if no output is provided, use input as output (replace the file)
     if (lzo_init() != LZO_E_OK) {
         printf("Error: failed to initialize LZO\n");
